a858e25d7eb1946741f90f4e9fc2c4c33b780517
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-08-07 19:58:48 +0500

    Refactor server component to incorporate OpenAI jokes
    The server component of our application has been refactored to utilize rendered jokes from the OpenAI module. Imports were re-organized, and dedicated variables for headers and template were introduced to enhance readability. These changes aim to enhance user experience by introducing humor through AI-based jokes.

--- api/index.mjs
+++ api/index.mjs
@@ -1,9 +1,13 @@
-import {stream} from "lit-edge-utils/render.mjs";
-import {render} from "@lit-labs/ssr";
-import {html} from "lit";
 import "open-ai-joke";
+import {html} from "lit";
+import {render} from "lit-async/lib/render.js";
+import {stream} from "lit-edge-utils/render.mjs";
+
+const headers = {"Content-Type": "text/html;charset=UTF-8"};
+
+const template = html`
+    <open-ai-joke subject="Why Lit does not yet support async SSR rendering ?"></open-ai-joke>`;
+
+export default () => new Response(stream(render(template)), {headers});
 
 export const config = {runtime: "edge"};
-
-export default () => new Response(stream(render(html`
-    <open-ai-joke subject="Why Lit does not yet support async SSR rendering ?"></open-ai-joke>`)));
